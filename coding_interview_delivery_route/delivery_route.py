689a1fe01780c8c8a9e4c879bdabaf78c9b6b338
1984MRB <fraguel_rock22@hotmail.com>
2020-07-04 08:59:39 +0100

    Added truck delivery problem

--- coding_interview_delivery_route/delivery_route.py
+++ coding_interview_delivery_route/delivery_route.py
@@ -7,6 +7,12 @@ class Cell:
     def hashkey(self):
         return str(self.row)+str(self.col)
 
+    def __eq__(self, other):
+        return self.row == other.row and self.col == other.col
+
+    def __str__(self):
+        return str(self.row)+str(self.col)
+
 def build_graph(area):
     graph = {}
     for row in range(len(area)):
@@ -34,3 +40,29 @@ def build_graph(area):
 
     return graph
 
+def find_route(area):
+    area_graph = build_graph(area)
+
+    # Start at the 0,0 cell
+    current_cell = Cell(0,0)
+    already_visited_list = [current_cell]
+    path =[current_cell]
+
+    while(area[current_cell.row][current_cell.col]!=2):
+        current_cell = move_next_available(current_cell,already_visited_list,area_graph)
+        if(not current_cell):
+            # the truck got to dead end, go back
+            current_cell = already_visited_list[len(already_visited_list)-2]
+            path.pop()
+        else:
+            already_visited_list.append(current_cell)
+            path.append(current_cell)
+
+    return len(path)-1
+
+def move_next_available(current_cell, alreadyVisitedCells, area_graph):
+    for adjacent_cell in area_graph[current_cell.hashkey()]:
+        if(not (adjacent_cell in alreadyVisitedCells)):
+            return adjacent_cell
+
+    return None
--- coding_interview_delivery_route/delivery_route_test.py
+++ coding_interview_delivery_route/delivery_route_test.py
@@ -1,8 +1,16 @@
 import unittest
-from coding_interview_delivery_route.delivery_route import build_graph, Cell
+from coding_interview_delivery_route.delivery_route import build_graph, Cell, find_route
 
 class DeliveryRouteTest(unittest.TestCase):
     def test_build_graph(self):
         graph = build_graph([[1,1,0],[0,1,2],[0,1,1]])
         self.assertEqual(1, len(graph.get(Cell(0,0).hashkey())))
         self.assertEqual(3, len(graph.get(Cell(1,1).hashkey())))
+
+    def test_case2(self):
+        min_distance = find_route([[1,1,0],[0,1,2],[0,1,1]])
+        self.assertEqual(5,min_distance)
+
+    def test_case3(self):
+        min_distance = find_route([[1,1,1,1],[0,1,0,1],[0,1,0,1],[0,1,1,0],[0,1,2,1]])
+        self.assertEqual(6, min_distance)
